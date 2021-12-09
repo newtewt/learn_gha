918a148692933060ffa718fe1bec7059576644b6
Nick Christenson <nick@fireworkhq.com>
2021-12-09 13:13:36 -0800

    run the job

--- label_action/index.js
+++ label_action/index.js
@@ -1,14 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const labeler = require('./labeler')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const issue = JSON.stringify(github?.context?.payload?.issue, undefined, 2)
+  const labels = labeler.labelsByPattern(issue.body, 'What platform to execute')
+  const repoLabels = await labeler.repoLabels()
+  const filteredLabels = labeler.filterLabels(repoLabels, labels)
+  labeler.addLabelsToIssue(issueNum, filteredLabels)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
