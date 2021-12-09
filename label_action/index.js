41cb01592cffa0057bfe5750808e3d7d5ded5135
Nick Christenson <nick@fireworkhq.com>
2021-12-09 13:45:18 -0800

    do

--- label_action/index.js
+++ label_action/index.js
@@ -3,9 +3,9 @@ const github = require('@actions/github');
 const labeler = require('./labeler')
 
 const doWork = async () => {
-  console.log(`The event payload: ${JSON.stringify(github.context.payload)}`);
-  const issue = JSON.stringify(github.context.payload.issue, undefined, 2)
-  const labels = labeler.labelsByPattern(issue.body, 'What platform to execute')
+  console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
+  const labels =  await labeler.labelsByPattern(issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
   labeler.addLabelsToIssue(issueNum, filteredLabels)
