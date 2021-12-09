c54a8a50ad7ad4cd3703252d49ffece5106fa4c4
Nick Christenson <nick@fireworkhq.com>
2021-12-09 13:50:22 -0800

    more console

--- label_action/dist/index.js
+++ label_action/dist/index.js
@@ -8580,7 +8580,10 @@ const labeler = __nccwpck_require__(3129)
 
 const doWork = async () => {
   console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
+  console.log(`The event issue body: ${JSON.stringify(github.context.payload.issue.body, undefined, 2)}`);
   const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
+  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
   const labels =  await labeler.labelsByPattern(issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
--- label_action/index.js
+++ label_action/index.js
@@ -4,7 +4,10 @@ const labeler = require('./labeler')
 
 const doWork = async () => {
   console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
+  console.log(`The event issue body: ${JSON.stringify(github.context.payload.issue.body, undefined, 2)}`);
   const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
+  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
   const labels =  await labeler.labelsByPattern(issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
