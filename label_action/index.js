c3fc235591f8818cf1e30ff4c8986af9592e5df9
Nick Christenson <nick@fireworkhq.com>
2021-12-09 16:16:56 -0800

    the whole thing

--- label_action/index.js
+++ label_action/index.js
@@ -8,7 +8,7 @@ const doWork = async () => {
   console.log(`The event issue body: ${JSON.stringify(github.context.payload.issue.body, undefined, 2)}`);
   const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
   console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
-  const labels =  await labeler.labelsByPattern(issue.body, 'What platform to execute')
+  const labels =  await labeler.labelsByPattern(github.context.payload.issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
   labeler.addLabelsToIssue(issueNum, filteredLabels)
