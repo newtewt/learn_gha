45ec7ec3bf8cf735d00da5a0aa5b65629d5db17e
Nick Christenson <nick@fireworkhq.com>
2021-12-09 16:24:52 -0800

    right json path

--- label_action/index.js
+++ label_action/index.js
@@ -11,7 +11,7 @@ const doWork = async () => {
   const labels =  await labeler.labelsByPattern(github.context.payload.issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
-  await labeler.addLabelsToIssue(github.context.payload.issue.body.number, filteredLabels)
+  await labeler.addLabelsToIssue(github.context.payload.issue.number, filteredLabels)
   
 }
 
