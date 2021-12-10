69a38205d7a20628f72a52c1ac0c42a6b92542b3
Nick Christenson <nick@fireworkhq.com>
2021-12-09 16:37:17 -0800

    npm remove some log

--- label_action/dist/index.js
+++ label_action/dist/index.js
@@ -8583,11 +8583,11 @@ const github = __nccwpck_require__(2771);
 const labeler = __nccwpck_require__(3129)
 
 const doWork = async () => {
-  console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
-  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
-  console.log(`The event issue body: ${JSON.stringify(github.context.payload.issue.body, undefined, 2)}`);
-  const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
-  console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
+  // console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  // console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
+  // console.log(`The event issue body: ${JSON.stringify(github.context.payload.issue.body, undefined, 2)}`);
+  // const issue = JSON.stringify(await github.context.payload.issue, undefined, 2)
+  // console.log(`The event issue: ${JSON.stringify(github.context.payload.issue, undefined, 2)}`);
   const labels =  await labeler.labelsByPattern(github.context.payload.issue.body, 'What platform to execute')
   const repoLabels = await labeler.repoLabels()
   const filteredLabels = labeler.filterLabels(repoLabels, labels)
